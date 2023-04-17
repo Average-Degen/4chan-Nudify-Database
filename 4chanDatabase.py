8ea4af0a038704504c6a524abbf98f0ab8bbd653
Average-Degen <130757028+Average-Degen@users.noreply.github.com>
2023-04-17 19:19:05 +1000

    Partially rewritten and fixed performance

--- 4chanDatabase.py
+++ 4chanDatabase.py
@@ -7,7 +7,6 @@ from PIL import Image
 import numpy as np
 import imagehash
 import requests
-import hashlib
 import string
 import random
 import shutil
@@ -16,25 +15,29 @@ import cv2
 import os
 import re
 
-def CheckAllHashes():
-    file_list = [f for f in listdir(
-        images_path) if isfile(join(images_path, f))]
+# unused function to write hash of all images in database folder
+# def CheckAllHashes():
+#     # get list of images
+#     file_list = [f for f in listdir(
+#         images_path) if isfile(join(images_path, f))]
 
-    hashes = []
-    for file in file_list:
-        if not "_NUDE" in file:
-            StoreHashes(imagehash.average_hash(Image.open(images_path + file)), file.split(".")[0])
+#     # append every image to hashes file
+#     hashes = []
+#     for file in file_list:
+#         if not "_NUDE" in file:
+#             StoreHashes(imagehash.average_hash(
+#                 Image.open(images_path + file)), file.split(".")[0])
 
 
+# write hash to file
 def StoreHashes(hash, file_name):
     with open(hash_file, "a") as f:
         f.write(str(hash) + ":" + file_name + "\n")
 
-def GetHash(file):
-    return imagehash.average_hash(Image.open(file))
 
-def CompareHashes(hash, name):
-    # read hashes of other files
+# check if hash is already stored
+def IsHashStored(hash, name):
+    # append hashes file contents to list
     cleaned_hashes = []
     with open(hash_file, "r") as f:
         hashes = f.readlines()
@@ -47,47 +50,53 @@ def CompareHashes(hash, name):
         if hash in x:
             hash_found = True
     if not hash_found:
-        # hash doesnt exist in database then add
+        # add to database then return that it isn't already in database
         StoreHashes(hash, name)
         return True
 
+
+# download image file from URL
 def DownloadImage(url, name):
     img_data = requests.get(url).content
     with open(f'{name}', 'wb') as f:
         f.write(img_data)
-    AddCheckedURLs(url)
+    AddCheckedURL(url)
 
-def AddCheckedURLs(url):
+
+# add URL to list of previously checked
+def AddCheckedURL(url):
     with open("CheckedURLs.txt", "a") as f:
         f.write(url + "\n")
 
-def FindNewThread():
+
+# find a thread to pull from
+def FindThread():
     full_thread = ""
     new_thread = ""
-    
+
     source = requests.get("https://boards.4chan.org/b/catalog")
     split_source = source.text.split("},")
     for x in split_source:
         if "nudify" in x.lower():
             # get url of thread/s
-            thread_index = split_source.index(x)    
-            id_index = split_source[thread_index -1]
+            thread_index = split_source.index(x)
+            id_index = split_source[thread_index - 1]
             id = id_index.split(":")[0]
             id = id.replace('"', "")
-            new_url = f"https://boards.4chan.org/b/thread/{id}"
-            
+            thread_url = f"https://boards.4chan.org/b/thread/{id}"
+
             try:
                 # check image count in thread
                 img_count = re.search('"i":(.*)', id_index)
                 if int(img_count.group(1).split(",")[0]) >= 149:
-                    full_thread = new_url
+                    full_thread = thread_url
                 else:
-                    return new_url
+                    new_thread = thread_url
             except:
                 # url not found
                 pass
     # check which thread to return
-    if new_thread == "" and full_thread != "":   
+    if new_thread == "" and full_thread != "":
         return full_thread
     elif new_thread != "":
         return new_thread
@@ -96,12 +105,15 @@ def FindNewThread():
         print("Sleeping for 60 seconds")
         time.sleep(60)
         return ""
+    
 
-# credit nathancy & bfontaine - stackoverflow 
+# check if images are related
+# credit nathancy & bfontaine - stackoverflow
 def DetectSimilar(file_name):
     first = cv2.imread(f'{file_name}.jpg')
     second = cv2.imread(f'{file_name}_NUDE.jpg')
 
+    # resize to uniform scale
     first = cv2.resize(first, (512, 512))
     second = cv2.resize(second, (512, 512))
 
@@ -111,17 +123,18 @@ def DetectSimilar(file_name):
 
     # Compute SSIM between two images
     score, diff = structural_similarity(first_gray, second_gray, full=True)
-    #print("Similarity Score: {:.3f}%".format(score * 100))
 
     # The diff image contains the actual image differences between the two images
-    # and is represented as a floating point data type so we must convert the array 
+    # and is represented as a floating point data type so we must convert the array
     # to 8-bit unsigned integers in the range [0,255] before we can use it with OpenCV
     diff = (diff * 255).astype("uint8")
 
     # Threshold the difference image, followed by finding contours to
     # obtain the regions that differ between the two images
-    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    thresh = cv2.threshold(
+        diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    contours = cv2.findContours(
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
 
     # Highlight differences
@@ -131,136 +144,135 @@ def DetectSimilar(file_name):
     for c in contours:
         area = cv2.contourArea(c)
         if area > 100:
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(first, (x, y), (x + w, y + h), (36,255,12), 2)
-            cv2.rectangle(second, (x, y), (x + w, y + h), (36,255,12), 2)
-            cv2.drawContours(mask, [c], 0, (0,255,0), -1)
-            cv2.drawContours(filled, [c], 0, (0,255,0), -1)
-    
-    # window displaying differences  
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(first, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.rectangle(second, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.drawContours(mask, [c], 0, (0, 255, 0), -1)
+            cv2.drawContours(filled, [c], 0, (0, 255, 0), -1)
+
+    # window displaying differences
     # cv2.imshow('diff', diff)
     # cv2.waitKey()
-    
+
     return score*100
 
-def FindReferences(url):
-    # get previously check URLs
+
+# check if URL has been checked previously
+def HasBeenChecked(url):
     prev_urls = []
     with open("CheckedURLs.txt", "r") as f:
-        prev_urls = f.readlines()
-    
+        for x in f.readlines():
+            prev_urls.append(x.replace("\n", ""))
+    return url in prev_urls
+
+
+# find all potential nudifies & originals 
+def FindPosts(url):
     source = requests.get(url)
     soup = BeautifulSoup(source.content, "html.parser")
-    
+
     # get posts in thread
     posts = soup.find_all("div", class_="post")
     for post in posts:
-        has_ref = False
-        has_img = False
-        
-        # find if post refs other post
+
+        # find if post is a reply
         quote = post.find("blockquote")
         try:
-            ref = quote.find("a")["href"]
-            has_ref = True
+            nfy_ref = quote.find("a")["href"]
         except:
-            has_ref = False
-            pass
-        
-        # find if post contains image
-        try:
-            img = post.find("a", class_="fileThumb")["href"]
-            has_img = True
-        except:
-            has_img = False
-            pass
-        
-        
-        # if image is nudified reply
-        if has_ref and has_img:
-            # create name for files
-            letters = string.ascii_lowercase
-            rand_name = ''.join(random.choice(letters) for i in range(20))
-            
-            
-            # download referenced non-nude image
-            reffed = soup.find_all(class_="post")
-            for post in reffed:
-                try:
-                    if post["id"] == ref.replace("#", ""):
-                        img_url = post.find("a", class_="fileThumb")["href"]
-                except:
-                    pass   
-            try:
-                if not "https:" + img_url in prev_urls and not "https:" + img in prev_urls:
-                    try:
-                        # download requested image
-                        DownloadImage(
-                            url = "https:" + img_url,
-                            name = ref.replace("#p", "") + ".jpg"
-                        )
-                        
-                        # download nudified version
-                        DownloadImage(
-                            url = "https:" + img,
-                            name = ref.replace("#p", "") + "_NUDE.jpg"
-                            )
-                        
-                        path = ref.replace("#p", "")
+            continue
 
-                        # if images are related
-                        similarity = DetectSimilar(ref.replace("#p", ""))
-                        if  similarity >=50:
-                            hash = str(GetHash(path + ".jpg"))
-                            is_new = CompareHashes(hash, path)
-                            if is_new:
-                                shutil.copy(path + ".jpg", f"ImageDatabase\\{path}.jpg")
-                                shutil.copy(path + "_NUDE.jpg", f"ImageDatabase\\{path}_NUDE.jpg")
-                                
-                                print("Image pair added to database!")
-                                
-                                # print(similarity)
-                                # print(ref.replace("#p", ""))
-                                # print("https:" + img_url) # original
-                                # print("https:" + img) # nudified
-                                # print("-------------------------------------------------")
-                        else:
-                            # print("Removed: " + ref.replace("#p", "") + ".jpg")
-                            # print("-------------------------------------------------")
-                            pass
-                    except:
-                        # one of them didnt have an image and I'm lazy
-                        pass
-                    os.remove(ref.replace("#p", "") + ".jpg")
-                    os.remove(ref.replace("#p", "") + "_NUDE.jpg")
+        # find if reply contains images
+        try:
+            nfy_img = post.find("a", class_="fileThumb")["href"]
+        except:
+            continue
+
+        # find image from original post
+        reffed = soup.find_all(class_="post")
+        for post in reffed:
+            try:
+                if post["id"] == nfy_ref.replace("#", ""):
+                    orig_img = post.find("a", class_="fileThumb")["href"]
             except:
-                # the amount of nested loops/error handlers is getting scary now
-                pass
-            
-        
+                continue
+
+        try:
+            url1_checked = HasBeenChecked("https:" + orig_img)
+            url2_checked = HasBeenChecked("https:" + nfy_img)
+
+            if not url1_checked and not url2_checked:
+                try:
+                    # download requested image
+                    DownloadImage(
+                        url="https:" + orig_img,
+                        name=nfy_ref.replace("#p", "") + ".jpg"
+                    )
+
+                    # download nudified version
+                    DownloadImage(
+                        url="https:" + nfy_img,
+                        name=nfy_ref.replace("#p", "") + "_NUDE.jpg"
+                    )
+
+                    path = nfy_ref.replace("#p", "")
+
+                    # if images are related
+                    similarity = DetectSimilar(nfy_ref.replace("#p", ""))
+                    if similarity >= 50:
+                        hash = str(imagehash.average_hash(Image.open(path + ".jpg")))
+                        is_new = IsHashStored(hash, path)
+                        if is_new:
+                            shutil.copy(path + ".jpg",
+                                        f"ImageDatabase\\{path}.jpg")
+                            shutil.copy(path + "_NUDE.jpg",
+                                        f"ImageDatabase\\{path}_NUDE.jpg")
+
+                            print("Image pair added to database!")
+
+                            # print(similarity)
+                            # print(ref.replace("#p", ""))
+                            # print("https:" + img_url) # original
+                            # print("https:" + img) # nudified
+                            # print("-------------------------------------------------")
+                    else:
+                        # print("Removed: " + ref.replace("#p", "") + ".jpg")
+                        # print("-------------------------------------------------")
+                        pass
+                except:
+                    # one of them didnt have an image and I'm lazy
+                    pass
+                os.remove(nfy_ref.replace("#p", "") + ".jpg")
+                os.remove(nfy_ref.replace("#p", "") + "_NUDE.jpg")
+        except:
+            pass
+
+
 images_path = "ImageDatabase\\"
 hash_file = "hashes.txt"
-has_URL = False
 
 open("CheckedURLs.txt", "w")
 
 while True:
-    os.system('cls' if os.name=='nt' else 'clear')
-    
+    # clear output
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     tprint("AVERAGE     DEGEN")
     print("**DO NOT CLOSE PROGRAM UNTIL TOLD SO**")
     print()
-    
-    thread_url = FindNewThread()
-    if "!DOCTYPE" in thread_url:
+
+    # find thread and check if it valid
+    thread_url = FindThread()
+    if "!DOCTYPE" in thread_url or thread_url == "":
         continue
     print("Accessing thread: " + thread_url)
     print()
-    
-    FindReferences(thread_url)
-    
+
+    FindPosts(thread_url)
+
     print()
     print("Sleeping for 60 seconds")
     print("Program can be safely closed if desired")
     print("---------------------------------------------------------------")
     time.sleep(60)
+
